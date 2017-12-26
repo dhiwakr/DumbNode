344b72d46dbee856a4c872b5176dd1d0c855ca11
Dhiwakar <dhiwakr@gmail.com>
2017-12-26 13:42:04 +0530

    Initial commit

---
+++ testnode.js
@@ -0,0 +1,4 @@
+console.log('Inside Node 1');
+var node2 = require('./testnode02.js')
+console.log('Inside Node 1');
+node2();
---
+++ testnode02.js
@@ -0,0 +1,6 @@
+console.log('Inside node 02');
+var a = -99
+
+exports.a = a;
+exports.b = -88;
+exports.c = -77;
